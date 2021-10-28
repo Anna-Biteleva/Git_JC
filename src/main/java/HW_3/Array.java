172b3d51cae3ad5d2883cd56c3244393509f2366
Anna-Biteleva <biteleva.anna@gmail.com>
2021-10-28 21:50:30 +0300

    Homework_3

---
+++ src/main/java/HW_3/Array.java
@@ -0,0 +1,17 @@
+package HW_3;
+
+import java.util.Arrays;
+
+public class Array {
+
+    public static void main(String[] args) {
+       Integer[] intArr = {1,3,5 };
+        System.out.println(Arrays.toString(intArr));
+       int i = intArr[2];
+       intArr[2] = intArr[1];
+       intArr[1] = i;
+       System.out.println(Arrays.toString(intArr));
+
+    }
+
+}
